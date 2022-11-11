eda382980ba8c61eb9388d221942fe69b226e142
Skjuber <ante.22@hotmail.com>
2022-11-11 17:01:31 +0100

    Redundant code deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PageLayout from "./components/PageLayout";
 import usePosts from "./components/utils/usePosts";
-import Homepage from "./components/Pages/HomePage/homePage";
-
-import AboutPage from "./components/Pages/AboutPage/aboutPage";
-
 import useUsers from "./components/utils/useUsers";
+import Homepage from "./components/Pages/HomePage/homePage";
+import AboutPage from "./components/Pages/AboutPage/aboutPage";
 import "./index.css";
 
 const App = () => {
